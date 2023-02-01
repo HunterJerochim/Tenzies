665ecea170231868daa7f1ba271f914b75f526d4
Stephen Fairris <31079321+stephanis1494@users.noreply.github.com>
2023-02-01 11:41:18 -0800

    fix some issues with setting data to local storage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Confetti from 'react-confetti'
 
 function App() {
 
- 
+
  const [dice, setDice] = React.useState(allNewDice())
  const [tenzies, setTenzies] = React.useState(false)
  const [renderCount, setRenderCount] = React.useState(0)
@@ -19,10 +19,10 @@ function App() {
  const [highscore, setHighscore] = React.useState(null)
 
 
- 
- 
+
+
  React.useEffect(() => {
-  let storedHighscore = JSON.parse(localStorage.getItem('highscore'));
+  let storedHighscore = JSON.parse(localStorage.getItem('tenzies-highscore'));
   if (storedHighscore) {
     setHighscore(storedHighscore);
   }
@@ -30,22 +30,18 @@ function App() {
 
 
 function updateHighscore() {
-  if (tenzies) {
     const currentTime = minutes * 60 + seconds;
-    if (!highscore || currentTime < highscore.minutes * 60 + highscore.seconds) {
+    const highScoreInSeconds = highscore?.minutes ? (highscore.minutes * 60) + highscore.seconds : 0
+
+    // don't set a time of zero
+    if (currentTime === 0) return
+
+    if (!highScoreInSeconds || currentTime < highScoreInSeconds) {
       setHighscore({ minutes, seconds });
-      localStorage.setItem('highscore', JSON.stringify({ minutes, seconds }));
+      localStorage.setItem('tenzies-highscore', JSON.stringify({ minutes, seconds }));
     }
-  }
 }
-React.useEffect(() => {
-  if (tenzies) {
-    updateHighscore();
-  }
-}, [tenzies]);
- 
- 
- 
+
  let intervalId;
 
  React.useEffect(() => {
@@ -71,11 +67,11 @@ React.useEffect(() => {
  * Challenge: Check the dice array for these winning conditions:
  * 1. All dice are held, and
  * 2. all dice have the same value
- * 
+ *
  * If both conditions are true, set `tenzies` to true and log
  * "You won!" to the console
  */
-  
+
 
   React.useEffect(() => {
     const allDiceHeld = dice.every(die => die.isHeld === true)
@@ -85,10 +81,10 @@ React.useEffect(() => {
       updateHighscore()
       console.log("You won!")
     }
- }, [dice, tenzies])
+ }, [dice])
 
- 
-  
+
+
   function generateNewDice() {
     return {
           value: Math.ceil(Math.random() * 6),
@@ -97,9 +93,9 @@ React.useEffect(() => {
     }
   }
 
- 
 
-  
+
+
   function allNewDice() {
   const newDice = [];
   for (let i = 0; i < 10; i++) {
@@ -132,18 +128,23 @@ function rollDice() {
   }))
   }
 }
-  
+
+function clearHighscore() {
+  localStorage.removeItem('tenzies-highscore')
+    setHighscore(null)
+}
+
   const diceElements = dice.map(dice => <Die key={dice.id} value={dice.value} isHeld={dice.isHeld} handleClick={() => holdDice(dice.id)} />)
-  
+
   return (
     <div>
       <div className="info-container">
         <div className="highscore-container">
-        <h2 className="highscore">Highscore:</h2>
+        <h2 className="highscore">{highscore !== null ? `Highscore: ${highscore?.minutes}:${highscore?.seconds}` : 'none' }</h2>
         </div>
       <div className="timer-container">
       <h2 className="timer">{tenzies ? "00:00" : `${minutes}:${seconds}`}</h2>
-      
+
       </div>
     <div className="roll-container">
       <h1 className="roll-counter">Rolls:{renderCount}</h1>
@@ -156,11 +157,17 @@ function rollDice() {
       <div className="die-container">
        {diceElements}
       </div>
-      <button 
-        className="roll-button" 
+      <button
+        className="roll-button"
         onClick={rollDice}>
         {tenzies ? "New Game" : "Roll"}
       </button>
+       <button
+           className="roll-button"
+           onClick={clearHighscore}
+       >
+           Clear Highscore
+       </button>
    </main>
    </div>
   );
